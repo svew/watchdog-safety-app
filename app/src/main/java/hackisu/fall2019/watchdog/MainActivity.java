5baa5b72c12bd58620d6da6ac32b893400cefbda
Arnoldo-linux-laptop <amontoya@iastate.edu>
2018-10-20 02:59:21 -0500

    Added very basic voice recognition, still need to experiment more

--- app/src/main/java/hackisu/fall2019/watchdog/MainActivity.java
+++ app/src/main/java/hackisu/fall2019/watchdog/MainActivity.java
@@ -1,7 +1,14 @@
 package hackisu.fall2019.watchdog;
 
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.speech.SpeechRecognizer.createSpeechRecognizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        SpeechRecognizer sr = createSpeechRecognizer(getApplicationContext());
+        final TextView textView = (TextView)findViewById(R.id.textView);
+        sr.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+                textView.setText("OnReadyForSpeech");
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                textView.setText("onBeginningOfSpeech");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+                textView.setText("onRmsChanged");
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+                textView.setText("onBufferReceived");
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                textView.setText("onEndOfSpeech");
+
+            }
+
+            @Override
+            public void onError(int i) {
+                textView.setText("onError: " + i);
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                textView.setText("onResults");
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+                textView.setText("onPartialResults");
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+                textView.setText("onEvent");
+
+            }
+        });
+        Intent recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,"en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        sr.startListening(recognizerIntent);
+
+            }
 }
