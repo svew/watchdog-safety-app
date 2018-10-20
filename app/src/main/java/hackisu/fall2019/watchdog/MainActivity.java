977bc48e6c54ad2e171564106448ac6b91ed8d7b
Jesrik <jgomez@iastate.edu>
2018-10-20 17:44:52 -0500

    Implemented logic for getting latitude and longitude from device

--- app/src/main/java/hackisu/fall2019/watchdog/MainActivity.java
+++ app/src/main/java/hackisu/fall2019/watchdog/MainActivity.java
@@ -3,11 +3,80 @@ package hackisu.fall2019.watchdog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private LocationManager locationManager;
+    private LocationListener listener;
+
+    private String link = "https://maps.google.com/?q=";
+
+    private TextView t;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t = (TextView) findViewById(R.id.textView);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        listener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                link += location.getLatitude() + "," + location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) { }
+
+            @Override
+            public void onProviderEnabled(String s) { }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+
+        // Location permission check
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // Request permission
+            requestPermissions(new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 10);
+            return;
+        }
+
+        locationManager.requestLocationUpdates("gps", 5000, 0, listener);
     }
 }
